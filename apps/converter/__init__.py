2aabf147277a51d3cbb8944f67a630a41ea79690
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-27 01:33:12 -0400

    Move converter app links to a seprate file

--- apps/converter/__init__.py
+++ apps/converter/__init__.py
@@ -8,11 +8,7 @@ from project_tools.api import register_tool
 
 from .utils import load_backend
 from .conf.settings import GRAPHICS_BACKEND
-
-def is_superuser(context):
-    return context['request'].user.is_staff or context['request'].user.is_superuser
-
-formats_list = {'text': _('file formats'), 'view': 'formats_list', 'famfam': 'pictures', 'icon': 'pictures.png', 'condition': is_superuser, 'children_view_regex': [r'formats_list']}
+from .links import formats_list
 
 register_sidebar_template(['formats_list'], 'converter_file_formats_help.html')
 
