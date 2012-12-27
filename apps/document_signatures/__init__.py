766d7f7aa0b57c2db7f4cfceda6b45cc2ebcb405
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-27 02:25:07 -0400

    Move document_signatures app links to a seprate file

--- apps/document_signatures/__init__.py
+++ apps/document_signatures/__init__.py
@@ -23,16 +23,9 @@ from .permissions import (
     PERMISSION_SIGNATURE_UPLOAD,
     PERMISSION_SIGNATURE_DOWNLOAD
 )
+from .links import document_signature_upload, document_signature_download, document_verify
 
 logger = logging.getLogger(__name__)
-
-
-def has_embedded_signature(context):
-    return DocumentVersionSignature.objects.has_embedded_signature(context['object'])
-
-
-def doesnt_have_detached_signature(context):
-    return DocumentVersionSignature.objects.has_detached_signature(context['object']) == False
 
 
 def document_pre_open_hook(descriptor, instance):
@@ -80,9 +73,6 @@ def document_post_save_hook(instance):
 #    if kwargs.get('created', False):
 #        DocumentVersionSignature.objects.signature_state(instance.document)
 
-document_signature_upload = {'text': _(u'upload signature'), 'view': 'document_signature_upload', 'args': 'object.pk', 'famfam': 'pencil_add', 'permissions': [PERMISSION_SIGNATURE_UPLOAD], 'conditional_disable': has_embedded_signature}
-document_signature_download = {'text': _(u'download signature'), 'view': 'document_signature_download', 'args': 'object.pk', 'famfam': 'disk', 'permissions': [PERMISSION_SIGNATURE_DOWNLOAD], 'conditional_disable': doesnt_have_detached_signature}
-document_verify = {'text': _(u'signatures'), 'view': 'document_verify', 'args': 'object.pk', 'famfam': 'text_signature', 'permissions': [PERMISSION_DOCUMENT_VERIFY]}
 
 register_links(Document, [document_verify], menu_name='form_header')
 register_links(['document_verify', 'document_signature_upload', 'document_signature_download'], [document_signature_upload, document_signature_download], menu_name='sidebar')
