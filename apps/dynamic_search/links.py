6d12955f1919e829167ed09b2573dd7106944a93
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-27 02:26:58 -0400

    Move dynamic_search app links to a seprate file

---
+++ apps/dynamic_search/links.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from django.utils.translation import ugettext_lazy as _
+
+search = {'text': _(u'search'), 'view': 'search', 'famfam': 'zoom'}
+search_advanced = {'text': _(u'advanced search'), 'view': 'search_advanced', 'famfam': 'zoom_in'}
+search_again = {'text': _(u'search again'), 'view': 'search_again', 'famfam': 'arrow_undo'}
