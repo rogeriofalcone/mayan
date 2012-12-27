7794402ccfb6abcd35ea20c0c545eab86b84af77
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-27 01:27:02 -0400

    Move common app links to a separate file

--- apps/common/__init__.py
+++ apps/common/__init__.py
@@ -19,22 +19,11 @@ from .conf.settings import (AUTO_CREATE_ADMIN, AUTO_ADMIN_USERNAME,
 from .conf import settings as common_settings
 from .utils import validate_path
 from .models import AutoAdminSingleton
-
-
-def has_usable_password(context):
-    return context['request'].user.has_usable_password
-
-password_change_view = {'text': _(u'change password'), 'view': 'password_change_view', 'famfam': 'computer_key', 'condition': has_usable_password}
-current_user_details = {'text': _(u'user details'), 'view': 'current_user_details', 'famfam': 'vcard'}
-current_user_edit = {'text': _(u'edit details'), 'view': 'current_user_edit', 'famfam': 'vcard_edit'}
+from .links import (password_change_view, current_user_details, current_user_edit,
+    about_view, license_view)
 
 register_links(['current_user_details', 'current_user_edit', 'password_change_view'], [current_user_details, current_user_edit, password_change_view], menu_name='secondary_menu')
-
-about_view = {'text': _('about'), 'view': 'about_view', 'famfam': 'information'}
-license_view = {'text': _('license'), 'view': 'license_view', 'famfam': 'script'}
-
 register_links(['about_view', 'license_view'], [about_view, license_view], menu_name='secondary_menu')
-
 register_top_menu('about', link={'text': _(u'about'), 'view': 'about_view', 'famfam': 'information'}, position=-1)
 
 
