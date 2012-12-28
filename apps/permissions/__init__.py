e31cbf403447e3c86007ec50f6f1b70efcecc649
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-28 00:44:51 -0400

    Move links from the permissions app to a seprate file

--- apps/permissions/__init__.py
+++ apps/permissions/__init__.py
@@ -10,19 +10,8 @@ from project_setup.api import register_setup
 
 from .conf.settings import DEFAULT_ROLES
 from .models import Role, Permission, PermissionNamespace
-from .permissions import (PERMISSION_ROLE_VIEW, PERMISSION_ROLE_EDIT,
-    PERMISSION_ROLE_CREATE, PERMISSION_ROLE_DELETE,
-    PERMISSION_PERMISSION_GRANT, PERMISSION_PERMISSION_REVOKE)
-
-role_list = {'text': _(u'roles'), 'view': 'role_list', 'famfam': 'medal_gold_1', 'icon': 'medal_gold_1.png', 'permissions': [PERMISSION_ROLE_VIEW], 'children_view_regex': [r'^permission_', r'^role_']}
-role_create = {'text': _(u'create new role'), 'view': 'role_create', 'famfam': 'medal_gold_add', 'permissions': [PERMISSION_ROLE_CREATE]}
-role_edit = {'text': _(u'edit'), 'view': 'role_edit', 'args': 'object.id', 'famfam': 'medal_gold_1', 'permissions': [PERMISSION_ROLE_EDIT]}
-role_members = {'text': _(u'members'), 'view': 'role_members', 'args': 'object.id', 'famfam': 'group_key', 'permissions': [PERMISSION_ROLE_EDIT]}
-role_permissions = {'text': _(u'role permissions'), 'view': 'role_permissions', 'args': 'object.id', 'famfam': 'key_go', 'permissions': [PERMISSION_PERMISSION_GRANT, PERMISSION_PERMISSION_REVOKE]}
-role_delete = {'text': _(u'delete'), 'view': 'role_delete', 'args': 'object.id', 'famfam': 'medal_gold_delete', 'permissions': [PERMISSION_ROLE_DELETE]}
-
-permission_grant = {'text': _(u'grant'), 'view': 'permission_multiple_grant', 'famfam': 'key_add', 'permissions': [PERMISSION_PERMISSION_GRANT]}
-permission_revoke = {'text': _(u'revoke'), 'view': 'permission_multiple_revoke', 'famfam': 'key_delete', 'permissions': [PERMISSION_PERMISSION_REVOKE]}
+from .links import (role_list, role_create, role_edit, role_members,
+    role_permissions, role_delete, permission_grant, permission_revoke)
 
 register_links(Role, [role_edit, role_delete, role_permissions, role_members])
 register_links([Role, 'role_list', 'role_create'], [role_list, role_create], menu_name='secondary_menu')
