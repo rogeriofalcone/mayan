8aa1d048f21712022d9dfc7a03bbb97789e1d6ad
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-28 00:48:22 -0400

    Move project_tools app links to a separate file

---
+++ apps/project_tools/links.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from django.utils.translation import ugettext_lazy as _
+
+tools_menu_link = {'text': _(u'tools'), 'view': 'tools_list', 'famfam': 'wrench'}
