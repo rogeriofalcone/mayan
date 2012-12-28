e742852b4faa1a7710b70ee268dd2760496a2e31
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-28 00:47:01 -0400

    Move links from the project_setup app to a separate file

--- apps/project_setup/__init__.py
+++ apps/project_setup/__init__.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import
+
 from django.utils.translation import ugettext_lazy as _
 
 from navigation.api import register_top_menu
 
-setup_link = register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setup_list', 'famfam': 'cog'}, position=-2)
+from .links import setup_menu_link
+
+setup_link = register_top_menu('setup_menu', link=setup_menu_link, position=-2)
---
+++ apps/project_setup/links.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from django.utils.translation import ugettext_lazy as _
+
+setup_menu_link = {'text': _(u'setup'), 'view': 'setup_list', 'famfam': 'cog'}
