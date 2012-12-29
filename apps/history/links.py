7a6ee065aed4c900714cc521a0e13b7c1a2c26ab
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-29 02:02:34 -0400

    Update history app to new navigation and icon system

--- apps/history/links.py
+++ apps/history/links.py
@@ -2,7 +2,10 @@ from __future__ import absolute_import
 
 from django.utils.translation import ugettext_lazy as _
 
+from navigation import Link
+
+from .icons import icon_history_list, icon_history_details
 from .permissions import PERMISSION_HISTORY_VIEW
 
-history_list = {'text': _(u'history'), 'view': 'history_list', 'famfam': 'book', 'icon': 'book.png', 'children_view_regex': [r'history_[l,v]']}
-history_details = {'text': _(u'details'), 'view': 'history_view', 'famfam': 'book_open', 'args': 'object.pk', 'permissions': [PERMISSION_HISTORY_VIEW]}
+history_list = Link(text=_(u'history'), view='history_list', icon=icon_history_list, permissions=[PERMISSION_HISTORY_VIEW], children_view_regex=[r'history_[l,v]'])
+history_details = Link(text=_(u'details'), view='history_view', icon=icon_history_details, args='object.pk', permissions=[PERMISSION_HISTORY_VIEW])
