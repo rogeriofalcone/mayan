7ecfe2635f601e3403f1ab1f2bf2faa531ba152f
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-30 01:41:50 -0400

    Update history app to use registry to register it's tool entry

--- apps/history/__init__.py
+++ apps/history/__init__.py
@@ -2,15 +2,12 @@ from __future__ import absolute_import
 
 from django.utils.translation import ugettext_lazy as _
 
-from project_tools.api import register_tool
 from navigation.api import register_model_list_columns, bind_links
 from common.utils import encapsulate
 
 from .models import History
 from .widgets import history_entry_summary, history_entry_type_link
 from .links import history_list, history_details
-    
-register_tool(history_list)
 
 register_model_list_columns(History, [
     {
