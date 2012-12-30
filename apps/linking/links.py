fb3f41e31d805f29e9d3927742485de28981019e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-30 03:48:25 -0400

    Update linking and tags apps to use new navigation and icons system

--- apps/linking/links.py
+++ apps/linking/links.py
@@ -2,25 +2,31 @@ from __future__ import absolute_import
 
 from django.utils.translation import ugettext_lazy as _
 
+from navigation.classes import Link
 from documents.permissions import PERMISSION_DOCUMENT_VIEW
 from acls.permissions import ACLS_VIEW_ACL
 
+from .icons import (icon_smart_link_instance_view, icon_smart_link_instances_for_document,
+    icon_smart_link_setup, icon_smart_link_list, icon_smart_link_create,
+    icon_smart_link_edit, icon_smart_link_delete, icon_smart_link_condition_list,
+    icon_smart_link_condition_create, icon_smart_link_condition_edit,
+    icon_smart_link_condition_delete, icon_smart_link_acl_list)
 from .permissions import (PERMISSION_SMART_LINK_VIEW,
     PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_DELETE,
     PERMISSION_SMART_LINK_EDIT)
 
-smart_link_instance_view_link = {'text': _(u'smart links actions'), 'view': 'smart_link_instance_view', 'famfam': 'page_link', 'permissions': [PERMISSION_DOCUMENT_VIEW]}
-smart_link_instances_for_document = {'text': _(u'smart links'), 'view': 'smart_link_instances_for_document', 'args': 'object.pk', 'famfam': 'page_link', 'permissions': [PERMISSION_DOCUMENT_VIEW]}
+smart_link_instance_view_link = Link(text=_(u'smart links actions'), view='smart_link_instance_view', icon=icon_smart_link_instance_view, permissions=[PERMISSION_DOCUMENT_VIEW])
+smart_link_instances_for_document = Link(text=_(u'smart links'), view='smart_link_instances_for_document', args='object.pk', icon=icon_smart_link_instances_for_document, permissions=[PERMISSION_DOCUMENT_VIEW])
 
-smart_link_setup = {'text': _(u'smart links'), 'view': 'smart_link_list', 'icon': 'link.png', 'permissions': [PERMISSION_SMART_LINK_CREATE], 'children_view_regex': [r'smart_link_list', 'smart_link_create', 'smart_link_delete', 'smart_link_edit', 'smart_link_condition_']}
-smart_link_list = {'text': _(u'smart links list'), 'view': 'smart_link_list', 'famfam': 'link', 'permissions': [PERMISSION_SMART_LINK_CREATE]}
-smart_link_create = {'text': _(u'create new smart link'), 'view': 'smart_link_create', 'famfam': 'link_add', 'permissions': [PERMISSION_SMART_LINK_CREATE]}
-smart_link_edit = {'text': _(u'edit'), 'view': 'smart_link_edit', 'args': 'object.pk', 'famfam': 'link_edit', 'permissions': [PERMISSION_SMART_LINK_EDIT]}
-smart_link_delete = {'text': _(u'delete'), 'view': 'smart_link_delete', 'args': 'object.pk', 'famfam': 'link_delete', 'permissions': [PERMISSION_SMART_LINK_DELETE]}
+smart_link_setup = Link(text=_(u'smart links'), view='smart_link_list', icon=icon_smart_link_setup, permissions=[PERMISSION_SMART_LINK_CREATE], children_view_regex=[r'smart_link_list', 'smart_link_create', 'smart_link_delete', 'smart_link_edit', 'smart_link_condition_'])
+smart_link_list = Link(text=_(u'smart links list'), view='smart_link_list', icon=icon_smart_link_list, permissions=[PERMISSION_SMART_LINK_CREATE])
+smart_link_create = Link(text=_(u'create new smart link'), view='smart_link_create', icon=icon_smart_link_create, permissions=[PERMISSION_SMART_LINK_CREATE])
+smart_link_edit = Link(text=_(u'edit'), view='smart_link_edit', args='object.pk', icon=icon_smart_link_edit, permissions=[PERMISSION_SMART_LINK_EDIT])
+smart_link_delete = Link(text=_(u'delete'), view='smart_link_delete', args='object.pk', icon=icon_smart_link_delete, permissions=[PERMISSION_SMART_LINK_DELETE])
 
-smart_link_condition_list = {'text': _(u'conditions'), 'view': 'smart_link_condition_list', 'args': 'object.pk', 'famfam': 'cog', 'permissions': [PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_CREATE]}
-smart_link_condition_create = {'text': _(u'create condition'), 'view': 'smart_link_condition_create', 'args': 'object.pk', 'famfam': 'cog_add', 'permissions': [PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_EDIT]}
-smart_link_condition_edit = {'text': _(u'edit'), 'view': 'smart_link_condition_edit', 'args': 'condition.pk', 'famfam': 'cog_edit', 'permissions': [PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_EDIT]}
-smart_link_condition_delete = {'text': _(u'delete'), 'view': 'smart_link_condition_delete', 'args': 'condition.pk', 'famfam': 'cog_delete', 'permissions': [PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_EDIT]}
+smart_link_condition_list = Link(text=_(u'conditions'), view='smart_link_condition_list', args='object.pk', icon=icon_smart_link_condition_list, permissions=[PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_CREATE])
+smart_link_condition_create = Link(text=_(u'create condition'), view='smart_link_condition_create', args='object.pk', icon=icon_smart_link_condition_create, permissions=[PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_EDIT])
+smart_link_condition_edit = Link(text=_(u'edit'), view='smart_link_condition_edit', args='condition.pk', icon=icon_smart_link_condition_edit, permissions=[PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_EDIT])
+smart_link_condition_delete = Link(text=_(u'delete'), view='smart_link_condition_delete', args='condition.pk', icon=icon_smart_link_condition_delete, permissions=[PERMISSION_SMART_LINK_CREATE, PERMISSION_SMART_LINK_EDIT])
 
-smart_link_acl_list = {'text': _(u'ACLs'), 'view': 'smart_link_acl_list', 'args': 'object.pk', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
+smart_link_acl_list = Link(text=_(u'ACLs'), view='smart_link_acl_list', args='object.pk', icon=icon_smart_link_acl_list, permissions=[ACLS_VIEW_ACL])
--- apps/tags/__init__.py
+++ apps/tags/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from django.utils.translation import ugettext_lazy as _
 
-from navigation.api import (register_links, register_top_menu,
+from navigation.api import (bind_links, register_top_menu,
     register_model_list_columns, register_multi_item_links)
 from common.utils import encapsulate
 from documents.models import Document
@@ -14,7 +14,8 @@ from taggit.managers import TaggableManager
 from .links import (tag_list, tag_create, tag_attach,
     tag_document_list, tag_delete, tag_edit, tag_tagged_item_list,
     tag_multiple_delete, tag_acl_list, tag_multiple_attach,
-    single_document_multiple_tag_remove, multiple_documents_selection_tag_remove)
+    single_document_multiple_tag_remove, multiple_documents_selection_tag_remove,
+    link_menu)
 from .widgets import (get_tags_inline_widget_simple, single_tag_widget)
 from .permissions import (PERMISSION_TAG_ATTACH, PERMISSION_TAG_REMOVE,
     PERMISSION_TAG_DELETE, PERMISSION_TAG_EDIT, PERMISSION_TAG_VIEW)
@@ -36,13 +37,13 @@ register_model_list_columns(Document, [
     },
 ])
 
-register_links(Tag, [tag_tagged_item_list, tag_edit, tag_delete, tag_acl_list])
+bind_links([Tag], [tag_tagged_item_list, tag_edit, tag_delete, tag_acl_list])
 register_multi_item_links(['tag_list'], [tag_multiple_delete])
-register_links([Tag, 'tag_list', 'tag_create'], [tag_list, tag_create], menu_name='secondary_menu')
-register_top_menu('tags', link={'text': _(u'tags'), 'view': 'tag_list', 'famfam': 'tag_blue'}, children_view_regex=[r'^tag_(list|create|delete|edit|tagged|acl)'])
+bind_links([Tag, 'tag_list', 'tag_create'], [tag_list, tag_create], menu_name='secondary_menu')
+register_top_menu('tags', link=link_menu)
 
-register_links(Document, [tag_document_list], menu_name='form_header')
-register_links(['document_tags', 'tag_remove', 'tag_multiple_remove', 'tag_attach'], [tag_attach], menu_name='sidebar')
+bind_links([Document], [tag_document_list], menu_name='form_header')
+bind_links(['document_tags', 'tag_remove', 'tag_multiple_remove', 'tag_attach'], [tag_attach], menu_name='sidebar')
 register_multi_item_links(['document_tags'], [single_document_multiple_tag_remove])
 
 register_multi_item_links(['document_find_duplicates', 'folder_view', 'index_instance_node_view', 'document_type_document_list', 'search', 'results', 'document_group_view', 'document_list', 'document_list_recent', 'tag_tagged_item_list'], [tag_multiple_attach, multiple_documents_selection_tag_remove])
