1c273c11a917097a885b23150b6ec55c4f0351e2
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-12-31 02:16:03 -0400

    Update document_acls app to use new navigation code

--- apps/document_acls/links.py
+++ apps/document_acls/links.py
@@ -3,5 +3,6 @@ from __future__ import absolute_import
 from django.utils.translation import ugettext_lazy as _
 
 from acls.permissions import ACLS_VIEW_ACL
+from navigation.classes import Link
 
-acl_list = {'text': _(u'ACLs'), 'view': 'document_acl_list', 'args': 'object.pk', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
+acl_list = Link(text=_(u'ACLs'), view='document_acl_list', args='object.pk', permissions=[ACLS_VIEW_ACL])
