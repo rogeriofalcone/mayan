d31b1c10c8f6ed13db9d02abc65314b4d6d597fb
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-01-01 05:38:55 -0400

    Provide a lightweight class for settings and move code from view to the api file

--- apps/smart_settings/api.py
+++ apps/smart_settings/api.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import
+
 from django.conf import settings as django_settings
 from django.utils.importlib import import_module
+
+from .classes import Setting
 
 settings = {}
 
@@ -46,3 +50,22 @@ def register_settings(namespace, module, settings):
             setting.get('description', u''),
             setting.get('hidden', False),
         )
+
+
+def get_settings():
+    new_settings = []
+    for namespace, sub_settings in settings.items():
+        for sub_setting in sub_settings:
+            if not sub_setting.get('hidden', False):
+                new_settings.append(
+                    Setting(
+                        module=sub_setting['module'],
+                        name=sub_setting['name'],
+                        global_name=sub_setting['global_name'],
+                        description=sub_setting.get('description', None),
+                        exists=sub_setting.get('exists', False),
+                        default=sub_setting['default'],
+                    )
+                )
+                
+    return new_settings
