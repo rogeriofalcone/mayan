f819ba9111b8e785a1e0e74772c11c24bb06810e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-01-02 12:19:04 -0400

    Add missing icons to the checkout app

---
+++ apps/checkouts/icons.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from icons.literals import (BASKET, BASKET_PUT, BASKET_REMOVE)
+from icons import Icon
+
+icon_checkout_list = Icon(BASKET)
+icon_check_out_document = Icon(BASKET_PUT)
+icon_check_in_document = Icon(BASKET_REMOVE)
--- apps/checkouts/links.py
+++ apps/checkouts/links.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from navigation.classes import Link
 
+from .icons import (icon_checkout_list, icon_check_out_document, icon_check_in_document)
 from .permissions import (PERMISSION_DOCUMENT_CHECKOUT, PERMISSION_DOCUMENT_CHECKIN, PERMISSION_DOCUMENT_CHECKIN_OVERRIDE)
 
 
@@ -15,12 +16,7 @@ def is_not_checked_out(context):
     return not context['object'].is_checked_out()
 
 
-checkout_list = Link(text=_(u'checkouts'), view='checkout_list')
-checkout_document = Link(text=_('check out document'), view='checkout_document', args='object.pk', condition=is_not_checked_out, permissions=[PERMISSION_DOCUMENT_CHECKOUT])
-checkin_document = Link(text=_('check in document'), view='checkin_document', args='object.pk', condition=is_checked_out, permissions=[PERMISSION_DOCUMENT_CHECKIN, PERMISSION_DOCUMENT_CHECKIN_OVERRIDE])
-checkout_info = Link(text=_('check in/out'), view='checkout_info', args='object.pk', children_views=['checkout_document', 'checkin_document'], permissions=[PERMISSION_DOCUMENT_CHECKIN, PERMISSION_DOCUMENT_CHECKIN_OVERRIDE, PERMISSION_DOCUMENT_CHECKOUT])
-
-# TODO: add icons to links
-#, sprite='basket'
-#sprite='basket_put', 
-#sprite='basket_remove', 
+checkout_list = Link(text=_(u'checkouts'), view='checkout_list', icon=icon_checkout_list)
+checkout_document = Link(text=_('check out document'), view='checkout_document', args='object.pk', condition=is_not_checked_out, permissions=[PERMISSION_DOCUMENT_CHECKOUT], icon=icon_check_out_document)
+checkin_document = Link(text=_('check in document'), view='checkin_document', args='object.pk', condition=is_checked_out, permissions=[PERMISSION_DOCUMENT_CHECKIN, PERMISSION_DOCUMENT_CHECKIN_OVERRIDE], icon=icon_check_in_document)
+checkout_info = Link(text=_('check in/out'), view='checkout_info', args='object.pk', children_views=['checkout_document', 'checkin_document'], permissions=[PERMISSION_DOCUMENT_CHECKIN, PERMISSION_DOCUMENT_CHECKIN_OVERRIDE, PERMISSION_DOCUMENT_CHECKOUT], icon=icon_checkout_list)
