5bff370ba1e778592e1ec74f9981feef27de84ef
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-01-06 01:59:45 -0400

    Convert dynamic search app to use new icon system instead of famfam sprites

--- apps/dynamic_search/links.py
+++ apps/dynamic_search/links.py
@@ -4,8 +4,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from navigation.classes import Link
 
-from .icons import icon_search
+from .icons import icon_search, icon_advanced_search, icon_search_again
 
-search = Link(text=_(u'search'), view='search', icon=icon_search)
-search_advanced = Link(text=_(u'advanced search'), view='search_advanced', icon=icon_search) # TODO: make different icon
-search_again = Link(text=_(u'search again'), view='search_again', icon=icon_search) # TODO: make different icon
+search = Link(text=_(u'search'), view='search', icon=icon_search, children_view_regex=[r'^search' , r'^result'])
+search_advanced = Link(text=_(u'advanced search'), view='search_advanced', icon=icon_advanced_search)
+search_again = Link(text=_(u'search again'), view='search_again', icon=icon_search_again)
