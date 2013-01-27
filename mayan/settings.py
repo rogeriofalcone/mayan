4230c7f811b2c265f6f5f3563ef4cb4980ec09d5
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-01-27 07:07:50 -0400

    document_storage, image_cache and gpg_home are now created inside site_media by default, site_media is usually setup as a persistent storage by PaaS providers

--- mayan/settings.py
+++ mayan/settings.py
@@ -78,7 +78,7 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'site_media/')
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'site_media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -89,7 +89,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
