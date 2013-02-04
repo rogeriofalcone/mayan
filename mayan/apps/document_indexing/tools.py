36617e5c3f5da2de9734abe0dc8e9447c05e6ebb
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-02-03 20:21:06 -0400

    Refactor index rebuilding code

--- mayan/apps/document_indexing/tools.py
+++ mayan/apps/document_indexing/tools.py
@@ -6,14 +6,6 @@ from .models import Index, IndexInstanceNode
 
 
 def do_rebuild_all_indexes():
-    #for index in Index.objects.all():
-    #    fs_delete_directory_recusive(index)
-
-    #IndexInstanceNode.objects.all().delete()
-    #DocumentRenameCount.objects.all().delete()
-    #for document in Document.objects.all():
-    #    update_indexes(document)
-
-    #return []  # Warnings - None
-    pass
-    # TODO: fix after Index manager code is stable
+    for document in Document.objects.all():
+        IndexInstanceNode.objects.delete_nodes_for(document=document)
+        Index.objects.update_for(document=document)
