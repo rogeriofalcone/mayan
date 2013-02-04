2c925a9d966c3dcd09dcf83bda1d47194a943f62
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-02-03 20:20:08 -0400

    Delete document index node instances first on cleanup to trigger the document indexing signals so that filesystem cleanup is executed as well

--- mayan/apps/document_indexing/cleanup.py
+++ mayan/apps/document_indexing/cleanup.py
@@ -1,7 +1,12 @@
 from __future__ import absolute_import
 
+from documents.models import Document
+
 from .models import Index
 
 
 def cleanup():
+    for document in Document.objects.all():
+        IndexInstanceNode.objects.delete_nodes_for(document=document)
+
     Index.objects.all().delete()
